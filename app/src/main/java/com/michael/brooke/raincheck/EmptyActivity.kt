33ecbae6f3ee8eab7c0b5fbc22c26fbb4393611e
Michael Brooke <michaelbrooke93@gmail.com>
2016-05-24 20:33:51 +0200

    Initial Commit

---
+++ app/src/main/java/com/michael/brooke/raincheck/EmptyActivity.kt
@@ -0,0 +1,13 @@
+package com.michael.brooke.raincheck
+
+import android.app.Activity
+import android.os.Bundle
+
+class EmptyActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+        return
+    }
+}
